4dd117d63d9d6a324ec3d8f99c9ec31cae4258ad
Abu <abylaikhanturmanov@gmail.com>
2025-02-28 15:31:50 +0000

    display info by gendiff -h

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,15 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Compares two configuration files and shows a difference."
+    )
+    parser.add_argument("first_file", help="First file to compare")
+    parser.add_argument("second_file", help="Second file to compare")
+    args = parser.parse_args()
+    
+    print("gendiff is running...")
+    print(f"Comparing {args.first_file} and {args.second_file}")
+
+if __name__ == "__main__":
+    main()
