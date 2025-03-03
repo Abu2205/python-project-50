9c9f83bbad01fd6dbca9cc13ebbf34f553dbc884
Abu <abylaikhanturmanov@gmail.com>
2025-03-03 15:40:51 +0000

    updated information gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,8 +4,13 @@ def main():
     parser = argparse.ArgumentParser(
         description="Compares two configuration files and shows a difference."
     )
-    parser.add_argument("first_file", help="First file to compare")
-    parser.add_argument("second_file", help="Second file to compare")
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
+    parser.add_argument(
+	'-f', '--format', 
+	help='set format of output',
+	default='stylish'
+    ) 
     args = parser.parse_args()
     
     print("gendiff is running...")
