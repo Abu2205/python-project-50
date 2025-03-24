7fb7d1f76894d33cc3db9f649ce3a74b1c3314d5
Abu <abylaikhanturmanov@gmail.com>
2025-03-24 12:24:35 +0000

    fixed workflow errors for diff_builder.py

--- gendiff/diff_builder.py
+++ gendiff/diff_builder.py
@@ -6,15 +6,15 @@ def build_diff(data1, data2):
         value1 = data1.get(key)
         value2 = data2.get(key)
 
-        if key in data1 and key in data2 and isinstance(value1, dict) and isinstance(
-            value2, dict):
-            diff.append({"key": key, "type": "nested", "children": build_diff(value1, 
-                                                                              value2)})
+        if key in data1 and key in data2 and isinstance(
+            value1, dict) and isinstance(value2, dict):
+            diff.append({"key": key, "type": 
+                         "nested", "children": build_diff(value1, value2)})
         elif key in data1 and key in data2 and value1 == value2:
             diff.append({"key": key, "type": "unchanged", "value": value1})
         elif key in data1 and key in data2:
-            diff.append({"key": key, "type": "changed", "old_value": value1, "new_value": 
-                         value2})
+            diff.append({"key": key, "type": 
+                         "changed", "old_value": value1, "new_value": value2})
         elif key in data1:
             diff.append({"key": key, "type": "removed", "value": value1})
         else:
